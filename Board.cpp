e4742d8e66a1f800dc02ab8f529529afd0a00fe4
linmjie <linmgjie@gmail.com>
2025-11-29 15:05:58 -0500

    found bitboards for each piece starting pos

---
+++ Board.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using ul = unsigned long;
+ul MAX = 562949953421311; //2^49 - 1
+
+int main(){
+ 	ul a = 12;
+	std::cout << a << '\n';
+	return 0;
+}
--- test.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-
-int main(){
-	std::cout << "Board Games are Cool" << '\n';
-	return 0;
-}
